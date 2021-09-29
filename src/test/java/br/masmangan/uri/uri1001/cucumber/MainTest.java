5f5f8f2b39d0e4f7082e3a080ff82b0dceaaebd7
Marco Mangan <marco.mangan@gmail.com>
2021-09-29 00:47:32 -0300

    #7

--- src/test/java/br/masmangan/uri/uri1001/cucumber/MainTest.java
+++ src/test/java/br/masmangan/uri/uri1001/cucumber/MainTest.java
@@ -1,22 +1,12 @@
 package br.masmangan.uri.uri1001.cucumber;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintStream;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.nio.charset.Charset;
 
 import org.junit.Test;
@@ -30,28 +20,27 @@ public class MainTest {
 		String actual;
 
 		InputStream inputStream = new ByteArrayInputStream(input.getBytes(Charset.forName("UTF-8")));
-		
+
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		PrintStream outputStream = new PrintStream(baos);
 
 		PrintStream previousOut = System.out;
 		InputStream previousIn = System.in;
-		
+
 		System.setIn(inputStream);
 		System.setOut(outputStream);
-		
+
 		Main.main(null);
-		
-		actual =  baos.toString();
-		
-		
+
+		actual = baos.toString();
+
 		assertEquals(expected, actual);
-		
+
 		inputStream.close();
 		outputStream.close();
-		
-	    System.setOut(previousOut);
-	    System.setIn(previousIn);
+
+		System.setOut(previousOut);
+		System.setIn(previousIn);
 
 	}
 
