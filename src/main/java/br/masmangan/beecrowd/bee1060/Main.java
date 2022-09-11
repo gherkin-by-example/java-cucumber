a0c3815fedc62fe9666493679cfd5e27e480e064
Quadros de Souza <lourenco.quadros.de.souza@hp.com>
2022-09-11 15:54:39 -0300

    change some code implementation

--- src/main/java/br/masmangan/beecrowd/bee1060/Main.java
+++ src/main/java/br/masmangan/beecrowd/bee1060/Main.java
@@ -7,23 +7,15 @@ import static java.lang.System.in;
 import static java.lang.System.out;
 
 public class Main {
-    public static ArrayList<Integer> numbers = new ArrayList<Integer>();
-
     private Main() {
 		
 	}
 
     public static void main(String[] args) {
-        numbers = readNumbers();
-        hasPositiveNumbers(numbers);
-    }
-
-    private static ArrayList<Integer> readNumbers () {
         Scanner scanner = new Scanner(in);
-
         ArrayList<Integer> inputedNumbers = new ArrayList<Integer>();
 
-        out.println(" ---- Type six numbers to start the program, write one a one, typing the number and pressing enter. ");
+        out.printf("Type six numbers to start the program, write one a one, typing the number and pressing enter.%n");
         
         inputedNumbers.add(scanner.nextInt());
         inputedNumbers.add(scanner.nextInt());
@@ -34,19 +26,13 @@ public class Main {
 
         scanner.close();
 
-        return inputedNumbers;
-    }
-
-    private static void hasPositiveNumbers (ArrayList<Integer> numbers) {
         int count = 0;
-
-        for (int i=0; i<numbers.size(); i++) {
-            if (numbers.get(i) >= 0) {
+        for (int i=0; i<inputedNumbers.size(); i++) {
+            if (inputedNumbers.get(i) >= 0) {
                 count++;
             }
         }
 
         out.printf("The list have %d positive numbers.%n", count);
-    }
-    
+    }    
 }
